6e25696497cb954dbc3fd552dc50b708fbbbf9ff
Jordan <Jordan@192.168.1.95>
2014-09-28 12:59:46 +0100

    First selection revision task.

---
+++ Selection revision task 1.py
@@ -0,0 +1,12 @@
+#Jordan Russell
+#28/09/14
+#Selection Class Exercise 1
+
+number_one = float(input("Please input your first number: "))
+number_two = float(input("Please input your second number: "))
+
+if (number_one == number_two):
+    print("Your two numbers are the same.")
+else:
+    print("Your two numbers are different.")
+    
